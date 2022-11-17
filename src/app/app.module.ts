9ce0eae61af89dea573f416371cb8b0790907a6c
nuttdguy <pygnasakdev@gmail.com>
2022-11-17 14:10:52 -0600

    add register form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,7 +34,13 @@ import { RegisterComponent } from './pages/register/register.component';
     LoginComponent,
     RegisterComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, CommonModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,21 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/models/user';
+import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ValidationErrors,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
-  user: User = new User();
+export class RegisterComponent {
+  username = new FormControl('', [Validators.required, Validators.email]);
+  firstname = new FormControl('', [Validators.required]);
+  lastname = new FormControl('', [Validators.required]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
+  ]);
 
-  constructor() {}
+  registerForm = new FormGroup({
+    username: this.username,
+    password: this.password,
+    firstname: this.firstname,
+    lastname: this.lastname,
+  });
 
-  ngOnInit(): void {}
+  showMsg = false;
+  msg = 'Please complete the form ...';
 
   register() {
-    this.user.role = 'user';
-
-    console.log(JSON.stringify(this.user) + '... is registered');
+    this.showMsg = true;
+    this.msg = 'Your account has been created.';
+    // console.log(this.registerForm);
   }
 }
